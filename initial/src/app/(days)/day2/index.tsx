1a861f1205d7e38ea45e44bf0b30735bcc7a2405
aniketjha9179 <aniketjha917928@gmail.com>
2023-12-28 18:44:56 +0530

    day2 onboarding screen

--- initial/src/app/(days)/day2/index.tsx
+++ initial/src/app/(days)/day2/index.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Stack.Screen   options={{title:'Day 2 onboarding'}} />
+      <Text>Day Details screen</Text>
+      <Button title='Go to onboarding'  />
     </View>
   )
 }
---
+++ initial/src/app/(days)/day2/onboarding.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onboarding = () => {
+  return (
+    <View>
+      <Text>onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
+
+const styles = StyleSheet.create({})
